65c818a2b6316ac93fb16c55e09d59cf3403ca88
cybergiant <leonardo.dessi18@gmail.com>
2021-12-22 11:17:44 -0800

    Nemici si muovono, definizione macro per gli id oggetto e rimozione di magic numbrer

--- main.c
+++ main.c
@@ -10,22 +10,26 @@
 #define MISSILE_2 2 // identificatore posizione Missile 2
 #define MAX_MISSILI 10 // Numero (pari) massimo di missili sparabili contemporaneamente
 
-// DA 200 a 300  identificatori per i missili
-// DA 11 a 100   identificatori Nemici
-// Da 101 a 150  identinficatori Bombe nemiche
+#define START_ID_MISSILI 200 // DA 200 a 300  identificatori per i missili
+#define END_ID_MISSILI 300
+#define START_ID_NEMICI 11 // DA 11 a 100   identificatori Nemici
+#define END_ID_NEMICI 100
+#define START_ID_BOMBE_NEMICHE 101 // Da 101 a 150  identinficatori Bombe nemiche
+#define END_ID_BOMBE_NEMICHE 150
+
 
 /* Struttura per la comunicazione tra figli e padre */
 typedef struct {
-    int n; /* soggetto che invia il dato: vespa o nave_player */
+    int id; /* soggetto che invia il dato: vespa o nave_player */
     int x; /* coordinata x */
     int y; /* coordinata y */
     int vite;
     int pid;
-    _Bool stato;
 } Pos;
 
+const Pos init = {0,0,0,0,0}; //Costante utilizzata per inizializzare la struttura pos
 _Noreturn void nave_player(int pipeout);
-void AreaGioco(int pipein);
+void AreaGioco(int pipein, int pipeout, Pos *array_nemici);
 
 int M; //Numero nemici
 int num_missili = 0;
@@ -87,7 +91,8 @@ int main() {
 }
 
 StatoCorrente gioco() {
-    int filedes[2];
+    int fd1[2];
+    int fd2[2];
     int pid_navicella;
     int i;
     pid_t pid_nemici = 1;
@@ -103,13 +108,20 @@ StatoCorrente gioco() {
     start_color();
     init_pair(0, COLOR_BLACK, COLOR_BLACK); // per cancellare
     init_pair(1, COLOR_WHITE, COLOR_BLACK); // per scrivere
+    init_pair(2, COLOR_RED, COLOR_BLACK); // per scrivere
 
     bkgd(COLOR_PAIR(1));
 
-    if (pipe(filedes) == -1) { //inizializzazione della pipe con AreaGioco degli errori
+    if (pipe(fd1) == -1) { //inizializzazione della pipe con AreaGioco degli errori
         perror("Errore nella creazione della pipe!");
         _exit(1);
     }
+    if (pipe(fd2) == -1) { //inizializzazione della pipe con AreaGioco degli errori
+        perror("Errore nella creazione della pipe!");
+        _exit(1);
+    }
+    if (fcntl(fd2[0], F_SETFL, O_NONBLOCK) < 0) //pipe non bloccante
+        exit(2);
 
     pid_navicella = fork(); //generazione di un secondo processo figlio per la nave_player
     switch (pid_navicella) {
@@ -119,19 +131,17 @@ StatoCorrente gioco() {
         case 0:
             prctl(PR_SET_NAME, (unsigned long) "Navicella");
             mvprintw(maxy / 2, maxx / 2, "%s", nave);
-            close(filedes[0]); /* chiusura del descrittore di lettura (standard input)*/
-            nave_player(
-                    filedes[1]); /* il secondo processo figlio invoca la funzione nave_player passandogli la pipe in scrittura*/
+            close(fd1[0]); /* chiusura del descrittore di lettura (standard input)*/
+            nave_player(fd1[1]); /* il secondo processo figlio invoca la funzione nave_player passandogli la pipe in scrittura*/
         default:    //processo padre
-            array_pos_nemici = (Pos *) malloc(sizeof(Pos) * M);
-            int temp;
+            array_pos_nemici = (Pos *) calloc(M, sizeof(Pos));
+            int temp, j=0, status = 0;
             for (i = 0; i < M; i++) {
                 pid_nemici = fork();
                 if (pid_nemici == -1) {
                     perror("Errore nell'esecuzione della fork.");
                     _exit(1);
                 } else if (pid_nemici == 0) {
-                    Pos temp_pos;
                     int vite;
                     char nome[10];
                     sprintf(nome, "Nemico_%d", i);
@@ -140,30 +150,39 @@ StatoCorrente gioco() {
                     array_pos_nemici[i].vite = 3;
                     array_pos_nemici[i].pid = getpid();
                     while (true) {
-                        //mettere read
-                        if(temp_pos.pid == getpid()){
-                            array_pos_nemici[i].vite = temp_pos.vite;
+                        read(fd2[0], array_pos_nemici, sizeof(array_pos_nemici));
+                        if(array_pos_nemici[i].vite > 0) {
+                            array_pos_nemici[i].y = (1 + (i % 5) * (DIM_NEMICO + 1)) + j;
+                            temp = (int) (i / 5) + 1;
+                            array_pos_nemici[i].x = maxx - temp * (DIM_NEMICO + 1);
+                            array_pos_nemici[i].id = START_ID_NEMICI + i;
+                            write(fd1[1], &array_pos_nemici[i], sizeof(array_pos_nemici[i]));
+                            usleep(1000000);
+                        } else {
+                            exit(1);
                         }
-                        array_pos_nemici[i].y = 1 + (i % 5) * (DIM_NEMICO + 1);
-                        temp = (int) (i / 5) + 1;
-                        array_pos_nemici[i].x = maxx - temp * (DIM_NEMICO + 1);
-                        array_pos_nemici[i].n = 11 + i;
-                        write(filedes[1], &array_pos_nemici[i], sizeof(array_pos_nemici[i]));
-                        usleep(1000000);
+                        if(j <= 4 && status == 0){
+                            j++;
+                            if(j == 4)
+                                status = 1;
+                        } else if (j >= 0 && status == 1){
+                            j--;
+                            if(j == 0)
+                               status = 0;
+                        }
                     }
                 } else if (pid_nemici != 0) {
                     continue;
                 }
             }
             prctl(PR_SET_NAME, (unsigned long) "Area_gioco");
-            close(filedes[1]); /* chiusura del descrittore di scrittura (standard output)*/
-            AreaGioco(filedes[0]); /* il processo padre invoca la funzione di AreaGioco */
-
+            close(fd1[1]); /* chiusura del descrittore di scrittura (standard output)*/
+            close(fd2[0]); /* chiusura del descrittore di lettura (standard input)*/
+            AreaGioco(fd1[0], fd2[1], array_pos_nemici); /* il processo padre invoca la funzione di AreaGioco */
     }
     /* siamo usciti dalla funzione di AreaGioco e vengono terminati i 2 processi figli e ripristinato il normale modo operativo dello schermo */
     kill(pid_navicella, 1);
     clear();
-    attron(COLOR_PAIR(1));
     printw("   _____              __  __   ______    ____   __      __  ______   _____    \n"
            "  / ____|     /\\     |  \\/  | |  ____|  / __ \\  \\ \\    / / |  ____| |  __ \\   \n"
            " | |  __     /  \\    | \\  / | | |__    | |  | |  \\ \\  / /  | |__    | |__) |  \n"
@@ -189,14 +208,14 @@ void nave_player(int pipeout) {
     Pos pos_navicella;
     _Bool blocco_sparo = false;
 
-    pos_navicella.n = NAVE;
+    pos_navicella.id = NAVE;
     pos_navicella.x = 1;
-    pos_navicella.y = maxy / 2;
+    pos_navicella.y = (maxy -DIM_NAVICELLA)/2;
 
     for(i=0; i< MAX_MISSILI; i++) {
         pos_missili[i].x = 0;
         pos_missili[i].y = 0;
-        pos_missili[i].n = 0;
+        pos_missili[i].id = 0;
         pos_missili[i].vite = 0;
         pos_missili[i].pid = 0;
     }
@@ -236,12 +255,12 @@ void nave_player(int pipeout) {
                     pos_missile2.y = pos_navicella.y + (DIM_NAVICELLA / 2);
                     pos_missile2.x = pos_navicella.x + DIM_NAVICELLA;
 
-                    pos_missile1.n = 200 + num_missili - 2;
+                    pos_missile1.id = START_ID_MISSILI + num_missili - 2;
                     pos_missile1.vite = 1;
-                    pos_missile2.n = 201 + num_missili - 2;
+                    pos_missile2.id = START_ID_MISSILI + num_missili - 1;
                     pos_missile2.vite = 1;
-                    i = 0;
 
+                    i = 0;
                     pid_missile1 = fork(); //generazione processo
                     switch (pid_missile1) {
                         case -1:
@@ -283,8 +302,6 @@ void nave_player(int pipeout) {
                                         }
                                         pos_missile2.x += 1;
                                         pos_missile2.vite = 1;
-                                        //mvprintw(2,15,"pos_missili[%d].vite = %d", pos_missile2.n - 200 ,pos_missile2.vite);
-                                        refresh();
                                         write(pipeout, &pos_missile2, sizeof(pos_missile2));
                                         write(filedes[1], &pos_missile2, sizeof(pos_missile2));
                                         i++;
@@ -306,8 +323,8 @@ void nave_player(int pipeout) {
         }
         for(i = 0; i<= MAX_MISSILI*100; i++) {
             read(filedes[0], &temp_missile, sizeof(temp_missile));
-            if ((temp_missile.n - 200) >= 0 && (temp_missile.n - 200) < MAX_MISSILI) {
-                pos_missili[temp_missile.n - 200].vite = temp_missile.vite;
+            if ((temp_missile.id - START_ID_MISSILI) >= 0 && (temp_missile.id - START_ID_MISSILI) < MAX_MISSILI) {
+                pos_missili[temp_missile.id - START_ID_MISSILI].vite = temp_missile.vite;
             }
         }
 
@@ -320,31 +337,30 @@ void nave_player(int pipeout) {
         if(num_missili == MAX_MISSILI && j == MAX_MISSILI) {
             num_missili = 0;
         }
-        mvprintw(0,15,"num_missili= %d, j= %d", num_missili, j);
-        refresh();
+        //mvprintw(0,15,"num_missili= %d, j= %d", num_missili, j);
     }
 }
 
 
 
 
-void AreaGioco(int pipein) {
+void AreaGioco(int pipein, int pipeout, Pos *pos_nemici) {
     int vite = 1000;
     int i, j;
     _Bool collision = false;
     Pos navicella, valore_letto;
-    Pos *pos_nemici = (Pos *) malloc(sizeof(Pos) * M);
     Pos missili[MAX_MISSILI];
     navicella.x = -1;
 
-
     for(i=0; i < MAX_MISSILI; i++) {
-        missili[i].x = 0;
-        missili[i].y = 0;
-        missili[i].n = 0;
-        missili[i].vite = 0;
-        missili[i].pid = 0;
+        missili[i] = init;
     }
+
+    attron(COLOR_PAIR(2));
+    for (i = 0; i < DIM_NAVICELLA; i++) {
+        mvaddstr((maxy -DIM_NAVICELLA)/2 + i, 1, nave[i]);
+    }
+    attroff(COLOR_PAIR(2));
 
     while (true) {
         /*if(getmaxx(stdscr)!= 80 || getmaxy(stdscr)!= 24){
@@ -353,39 +369,46 @@ void AreaGioco(int pipein) {
             clear();
             refresh();
         }*/
-
-        move(0, 1);
-        printw("VITE: %d", vite);
-        refresh();
         read(pipein, &valore_letto, sizeof(valore_letto)); /* leggo dalla pipe */
-        if (valore_letto.n == NAVE) {
+        if (valore_letto.id == NAVE) {
             if (navicella.x >= 0) { /* cancello la 'vecchia' posizione della navicella */
                 attron(COLOR_PAIR(0));
                 for (i = 0; i < DIM_NAVICELLA; i++) {
-                    mvprintw(navicella.y + i, navicella.x, "      ");
+                    mvaddstr(navicella.y + i, navicella.x, "      ");
                 }
-                attron(COLOR_PAIR(1));
+                attroff(COLOR_PAIR(0));
+                attron(COLOR_PAIR(2));
                 for (i = 0; i < DIM_NAVICELLA; i++) {
-                    mvprintw(valore_letto.y + i, valore_letto.x, nave[i]);
+                    mvaddstr(valore_letto.y + i, valore_letto.x, nave[i]);
                 }
+                attroff(COLOR_PAIR(2));
             }
             navicella = valore_letto;
-        } else if (valore_letto.n >= 200 && valore_letto.n <= 300) {
+        } else if (valore_letto.id >= START_ID_MISSILI && valore_letto.id <= END_ID_MISSILI) {
+            if(missili[valore_letto.id - START_ID_MISSILI].id != 0) {
                 attron(COLOR_PAIR(0));
-                mvprintw(missili[valore_letto.n - 200].y, missili[valore_letto.n - 200].x, " ");
-                attron(COLOR_PAIR(1));
-                mvprintw(valore_letto.y, valore_letto.x, ">"); ///♿ ⟢ ⁂ ꗇ ꗈ 💣 🚀
-                missili[valore_letto.n - 200] = valore_letto;
-        }
-
-        for(i=0; i < M; i++) {
-            if (valore_letto.n >= 11 && valore_letto.n <= 11 + M) {
-                attron(COLOR_PAIR(1));
-                for (j = 0; j < DIM_NEMICO; j++) {
-                    mvprintw(valore_letto.y+j, valore_letto.x, nemico_lv1[j]);
-                }
-                pos_nemici[i] = valore_letto;
+                mvprintw(missili[valore_letto.id - START_ID_MISSILI].y, missili[valore_letto.id - START_ID_MISSILI].x, " ");
+                attroff(COLOR_PAIR(0));
             }
+            attron(COLOR_PAIR(1));
+            mvprintw(valore_letto.y, valore_letto.x, ">"); ///♿ ⟢ ⁂ ꗇ ꗈ 💣 🚀
+            attroff(COLOR_PAIR(1));
+            missili[valore_letto.id - START_ID_MISSILI] = valore_letto;
+        } else if (valore_letto.id >= START_ID_NEMICI && valore_letto.id <= END_ID_NEMICI){
+            if (pos_nemici[valore_letto.id - START_ID_NEMICI].id != 0) {
+                attron(0);
+                for (j = 0; j < DIM_NEMICO; j++) {
+                    mvprintw(pos_nemici[valore_letto.id - START_ID_NEMICI].y + j,
+                             pos_nemici[valore_letto.id - START_ID_NEMICI].x, "   ");
+                }
+                attroff(COLOR_PAIR(0));
+            }
+            attron(0);
+            for (j = 0; j < DIM_NEMICO; j++) {
+                mvprintw(valore_letto.y + j, valore_letto.x, nemico_lv1[j]);
+            }
+            attroff(COLOR_PAIR(1));
+            pos_nemici[valore_letto.id - START_ID_NEMICI] = valore_letto;
         }
 
         /* visualizzo l'oggetto nella posizione aggiornata */
@@ -402,10 +425,11 @@ void AreaGioco(int pipein) {
 
             }*/
 
-        curs_set(0);
+        move(0,0);
+        clrtoeol();
+        mvprintw(0, 0,"VITE: %d", vite);
         refresh();
         if (vite == 0)
             collision = true;
     }
-    free(pos_nemici);
 }
